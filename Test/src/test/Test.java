8f725759907ab48e7a212b84dbe1ea8163b026ff
FlorisVandenBussche <florisvandenbussche97@gmail.com>
2017-03-28 14:34:52 +0200

    Test2

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -4,6 +4,11 @@ public class Test {
 
     public static void main(String[] args) {
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+        //steve heeft aids
+        //cadeautje van jihadi john
+>>>>>>> parent of 8f5e4f4... Test
         int i = 1;
 =======
         //steve heeft aids
