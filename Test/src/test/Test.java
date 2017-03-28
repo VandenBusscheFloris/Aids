bcd4545a1d9d20b0e8fc6866a8eac2c5e6bb19fc
VandenBusscheFloris <florisvandenbussche97@gmail.com>
2017-03-28 14:37:43 +0200

    Test3

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -3,17 +3,9 @@ package test;
 public class Test {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
         //steve heeft aids
         //cadeautje van jihadi john
->>>>>>> parent of 8f5e4f4... Test
         int i = 1;
-=======
-        //steve heeft aids
-        //cadeautje van jihadi john
->>>>>>> parent of f9d954f... Test
     }
     
 }
