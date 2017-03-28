0caaa2b0576935a79a9697680c5214c49c5eba95
Blussertje <michiel.jonkmans@student.howest.be>
2017-03-28 15:32:30 +0200

    interger naar 9

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -5,7 +5,7 @@ public class Test {
     public static void main(String[] args) {
         //steve heeft aids
         //cadeautje van jihadi john
-        int i = 5;
+        int i = 9;
     }   
     
 }
